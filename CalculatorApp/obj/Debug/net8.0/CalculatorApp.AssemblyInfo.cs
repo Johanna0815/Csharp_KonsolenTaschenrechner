5dad7353a6d57947bb9bb2c0d526e194eb6a6b20
johanna <if20b016@technikum-wien.at>
2024-09-27 23:08:16 +0200

    Added GitHub Actions CI workflow

--- CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
+++ CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be942514a2694b55eb9d610cf94ee241d4bf6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
